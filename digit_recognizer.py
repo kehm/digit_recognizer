11b71e8112e23ab5126d537487eec07dc5217387
kehm <kjellerik.marstein@gmail.com>
2018-10-23 18:36:29 +0200

    Converted to numpy arrays

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,10 +1,12 @@
 import csv
+import numpy as np
 
 
 def load_labels():
     """Returns array of labels"""
     labels = []
-    with open("resources/handwritten_digits_labels.csv", mode='r') as csv_file:
+    file = "resources/handwritten_digits_labels.csv"
+    with open(file, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             labels.append(row)
@@ -14,15 +16,19 @@ def load_labels():
 def load_images():
     """Returns array of images"""
     images = []
-
+    file = "resources/handwritten_digits_images.csv"
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            images.append(row)
     return images
 
 
 def main():
     """Main function"""
-    data_labels = load_labels()
-    data_images = load_images()
-    print(data_labels)
+    data_labels = np.asarray(load_labels())
+    data_images = np.asarray(load_images())
+    data_images = data_images.reshape(data_images.shape[0], 28, 28)
 
 
 if __name__ == '__main__':
