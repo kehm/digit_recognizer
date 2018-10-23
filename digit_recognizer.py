88b7eedff7dbb1234d2c4b913d7b2c4d0bf513e8
kehm <kjellerik.marstein@gmail.com>
2018-10-23 23:14:10 +0200

    Started implementing decision tree classifier

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,5 +1,8 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as pt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 
 def load_labels():
@@ -24,11 +27,25 @@ def load_images():
     return images
 
 
+def run_decision_tree_classifier(data_images, data_labels):
+    images_train, images_test, labels_train, labels_test = train_test_split(data_images, data_labels, test_size=0.25,
+                                                                            random_state=42)
+    clf = DecisionTreeClassifier()
+    #print(images_train[0])
+    #print(len(images_test))
+    images_test = np.asmatrix(images_test)
+    d = images_test[0]
+    d.shape = (28, 28)
+    pt.imshow(255-d, cmap='gray')
+    pt.show()
+
+
 def main():
     """Main function"""
     data_labels = np.asarray(load_labels())
     data_images = np.asarray(load_images())
     data_images = data_images.reshape(data_images.shape[0], 28, 28)
+    run_decision_tree_classifier(data_images, data_labels)
 
 
 if __name__ == '__main__':
