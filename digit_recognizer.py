043e60354c852c88cbc2472150ea65f132885443
kehm <kjellerik.marstein@gmail.com>
2018-10-24 14:46:35 +0200

    Implemented clf

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -27,21 +27,23 @@ def load_images():
     return images
 
 
-def run_decision_tree_classifier(data_images, data_labels):
-    images_train, images_test, labels_train, labels_test = train_test_split(data_images, data_labels, test_size=0.25,
-                                                                            random_state=42)
+def run_decision_tree_classifier(x_train, y_train, x_test, y_test):
+    """Trains a decision tree classifier"""
     clf = DecisionTreeClassifier()
-
-    pt.imshow(255-images_test[0], cmap='gray')
-    pt.show()
+    clf.fit(x_train, y_train)
+    #pt.imshow(255-images_test[0], cmap='gray')
+    #pt.show()
+    print(clf.predict([x_test[0]]))
 
 
 def main():
     """Main function"""
     data_labels = np.asarray(load_labels(), dtype='float64')
     data_images = np.asarray(load_images(), dtype='float64')
-    data_images = data_images.reshape(data_images.shape[0], 28, 28)
-    run_decision_tree_classifier(data_images, data_labels)
+    #data_images = data_images.reshape(data_images.shape[0], 28, 28)
+    images_train, images_test, labels_train, labels_test = train_test_split(data_images, data_labels, test_size=0.25,
+                                                                            random_state=42)
+    run_decision_tree_classifier(images_train, labels_train, images_test, labels_test)
 
 
 if __name__ == '__main__':
