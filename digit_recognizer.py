8d42db58fb23e0af846edf322120f2ed687855f6
kehm <kjellerik.marstein@gmail.com>
2018-10-26 00:08:04 +0200

    Changed timings

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -45,53 +45,64 @@ def show_image(image_array):
 
 def run_decision_tree_classifier(X_train, y_train, X_test, y_test):
     """Trains and tests a decision tree classifier"""
-    start = time.time()
     clf = DecisionTreeClassifier()
+    start_train = time.time()
     clf.fit(X_train, y_train.ravel())
+    end_train = time.time()
     y_predict = clf.predict(X_test)
+    end_test = time.time()
     score = accuracy_score(y_test, y_predict)
     print(clf.predict([X_test[0]]))
-    print("Decision Tree Classifier Accuracy:", score, "Running time:", time.time() - start)
+    print("Decision Tree Classifier Accuracy:", score, "Time spent training:", end_train - start_train,
+          "Time spent testing:", end_test - end_train)
 
 
 def run_random_forest_classifier(X_train, y_train, X_test, y_test):
     """Trains and tests a random forrest classifier"""
-    start = time.time()
     clf = RandomForestClassifier()
+    start_train = time.time()
     clf.fit(X_train, y_train.ravel())
+    end_train = time.time()
     y_predict = clf.predict(X_test)
+    end_test = time.time()
     score = accuracy_score(y_test, y_predict)
     print(clf.predict([X_test[0]]))
-    print("Random Forest Classifier Accuracy", score, "Running time:", time.time() - start)
+    print("Random Forest Classifier Accuracy:", score, "Time spent training:", end_train - start_train,
+          "Time spent testing:", end_test - end_train)
 
 
 def run_k_neighbors_classifier(X_train, y_train, X_test, y_test):
     """Trains and tests a K nearest neighbors classifier"""
-    start = time.time()
     clf = KNeighborsClassifier()
+    start_train = time.time()
     clf.fit(X_train, y_train.ravel())
+    end_train = time.time()
     y_predict = clf.predict(X_test)
+    end_test = time.time()
     score = accuracy_score(y_test, y_predict)
     print(clf.predict([X_test[0]]))
-    print("K Nearest Neighbors Classifier Accuracy", score, "Running time:", time.time() - start)
+    print("K Nearest Neighbors Classifier Accuracy:", score, "Time spent training:", end_train - start_train,
+          "Time spent testing:", end_test - end_train)
 
 
 def run_support_vector_machine(X_train, y_train, X_test, y_test):
     """Trains and tests a support vector machine"""
-    start = time.time()
     clf = SVC(kernel='linear', C=1E10)
+    start_train = time.time()
     clf.fit(X_train, y_train.ravel())
+    end_train = time.time()
     y_predict = clf.predict(X_test)
+    end_test = time.time()
     score = accuracy_score(y_test, y_predict)
     print(clf.predict([X_test[0]]))
-    print("Support Vector Machine Accuracy", score, "Running time:", time.time() - start)
+    print("Support Vector Machine Accuracy:", score, "Time spent training:", end_train - start_train,
+          "Time spent testing:", end_test - end_train)
 
 
 def run_neural_network(X_train, y_train, X_test, y_test):
     """Trains and tests a neural network"""
     y_train = np_utils.to_categorical(y_train, 10)
     y_test = np_utils.to_categorical(y_test, 10)
-    start = time.time()
     model = Sequential()
     model.add(Dense(512, input_shape=(784,), activation='relu'))
     model.add(Dropout(0.2))
@@ -99,9 +110,13 @@ def run_neural_network(X_train, y_train, X_test, y_test):
     model.add(Dropout(0.2))
     model.add(Dense(10, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    start_train = time.time()
     model.fit(X_train, y_train, epochs=8, batch_size=128, verbose=0, validation_data=(X_test, y_test))
+    end_train = time.time()
     score = model.evaluate(X_test, y_test, verbose=0)
-    print("Neural Network Accuracy", score, "Running time:", time.time() - start)
+    end_test = time.time()
+    print("Neural Network Accuracy:", score[1], "Time spent training:", end_train - start_train,
+          "Time spent testing:", end_test - end_train)
 
 
 def main():
@@ -110,10 +125,10 @@ def main():
     data_images = np.asarray(load_images(), dtype='float64')
     images_train, images_test, labels_train, labels_test = train_test_split(data_images, data_labels, test_size=0.25,
                                                                             random_state=42)
-    #show_image(images_test[0])
+    show_image(images_test[0])
     images_train /= 255
     images_test /= 255
-    #run_decision_tree_classifier(images_train, labels_train, images_test, labels_test)
+    run_decision_tree_classifier(images_train, labels_train, images_test, labels_test)
     #run_random_forest_classifier(images_train, labels_train, images_test, labels_test)
     #run_k_neighbors_classifier(images_train, labels_train, images_test, labels_test)
     #run_support_vector_machine(images_train, labels_train, images_test, labels_test)
